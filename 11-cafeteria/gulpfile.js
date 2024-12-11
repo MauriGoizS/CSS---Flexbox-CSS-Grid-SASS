0672c2dda45d2ed78703a576b3f76b1a93ffffe1
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2024-12-10 21:10:34 -0600

    Añadir Autoprefixr

--- 11-cafeteria/gulpfile.js
+++ 11-cafeteria/gulpfile.js
@@ -1,12 +1,15 @@
 const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'))
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
 function css(  done ){
     // Compilar sass
     // pasos: 1 - Identificar archivo, 2 - Compilarla, 3 - Guardar el .css
 
     src('src/scss/app.scss')
-        .pipe( sass({ style: 'compressed' }) ) // Para expandir, cambiarlo por  "expanded" 
+        .pipe( sass() ) // Para expandir, cambiarlo por  "expanded" 
+        .pipe( postcss([ autoprefixer() ]) )
         .pipe( dest('build/css') )
 
     done();
